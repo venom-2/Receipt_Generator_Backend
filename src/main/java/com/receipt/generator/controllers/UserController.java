94e2b4e8965ea0ea9ccf0c3181df633f04836a92
Akshat Trivedi <akshattrivedi32@gmail.com>
2024-11-27 12:26:45 +0530

    dockerfile

--- src/main/java/com/receipt/generator/controllers/UserController.java
+++ src/main/java/com/receipt/generator/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "https://receipt-generatorfrontend.vercel.app/" )
+@CrossOrigin(origins = "http://localhost:5173/" )
 public class UserController {
     @Autowired
     UserService userService;
