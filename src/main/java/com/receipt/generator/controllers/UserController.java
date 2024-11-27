639471c1993eb1e921a5cc7553d3ba03be492a32
GameOver2811 <kdwivedi343@gmail.com>
2024-11-27 16:16:22 +0530

    CORS resolved

--- src/main/java/com/receipt/generator/controllers/UserController.java
+++ src/main/java/com/receipt/generator/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "https://receipt-generatorfrontend.vercel.app/" )
+@CrossOrigin(origins = "*" )
 public class UserController {
     @Autowired
     UserService userService;
