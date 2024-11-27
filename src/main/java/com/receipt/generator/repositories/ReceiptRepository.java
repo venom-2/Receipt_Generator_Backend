32d92b3e3a293ca4f4e4a7c6536ee6983917f61e
GameOver2811 <kdwivedi343@gmail.com>
2024-11-27 13:45:39 +0530

    Added receipt Endpoints

--- src/main/java/com/receipt/generator/repositories/ReceiptRepository.java
+++ src/main/java/com/receipt/generator/repositories/ReceiptRepository.java
@@ -3,6 +3,14 @@ package com.receipt.generator.repositories;
 import com.receipt.generator.entities.Receipt;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ReceiptRepository extends MongoRepository<Receipt, String> {
     boolean existsByBillNumber(String billNumber);
+
+    List<Receipt> findAllByUser(String userId);
+
+    void deleteByBillNumber(String billNumber);
+
+    List<Receipt> findAllByCustomerPhone(String customerPhone);
 }
